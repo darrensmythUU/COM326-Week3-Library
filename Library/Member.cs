00cc9e590bc1eaba3afab933ca836da5d5f67309
Kiphea <djsmyth05@gmail.com>
2025-11-08 15:20:36 +0000

    Adding a new class to the library (and GitHub sync)

---
+++ Library/Member.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Member
+    {
+        // Private Fields
+        private int memberID;
+        private string name;
+        private string address;
+        private int phone;
+
+        // Get/Set Constructors
+        public int MemberID
+        {
+            get { return memberID; }
+            set { memberID = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        // Constructor for Member object
+        public Member(int memberMemberID, string memberName, string memberAddress, int memberPhone)
+        {
+            this.MemberID = memberMemberID;
+            this.Name = memberName;
+            this.Address = memberAddress;
+            this.Phone = memberPhone;
+        }
+
+        // Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {MemberID}");
+            Console.WriteLine($"Member Name: {Name}");
+            Console.WriteLine($"Member Address: {Address}");
+            Console.WriteLine($"Member Phone Number: {Phone}");
+            Console.WriteLine();
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -4,13 +4,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // Create a new instance (object) of the book class
+        // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
         Book book = new Book("C# for Beginners", "BillGates", "1234567");
-        book.DisplayInfo();
-
-        // This is another new book in our library
         Book book1 = new Book("C# Methods and Classes", "Microsoft", "2233445");
+        Console.WriteLine("Currently Available Books");
+        book.DisplayInfo();
         book1.DisplayInfo();
+
+        // Create a new instances of the Member class
+        // These new members are created using the Member constructor in the Members class
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current Library Members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
