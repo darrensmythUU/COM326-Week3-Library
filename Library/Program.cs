827aa2ee769ccbdc91e4a3160e5fdcbd32a40066
Kiphea <djsmyth05@gmail.com>
2025-11-08 15:04:51 +0000

    Access Modifiers (and Github sync)

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,9 +1,16 @@
 ﻿using Library;
 
-// This information is for one book in our library
-Book book = new Book("C# for beginners", "BillGates", "12345678");
-book.DisplayInfo();
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create a new instance (object) of the book class
+        // Note how the object name differs from the class name
+        Book book = new Book("C# for beginners", "BillGates", "12345678");
+        book.DisplayInfo();
 
-// This is another new book in our library
-Book book1 = new Book("C# Methods and Classes", "Microsoft", "55667788");
-book1.DisplayInfo();
+        // This is another new book in our library
+        Book book1 = new Book("C# Methods and Classes", "Microsoft", "55667788");
+        book1.DisplayInfo();
+    }
+}
