c3b897ab4ce624b17a8f3252bcaadb416a673ecb
Kiphea <djsmyth05@gmail.com>
2025-11-08 15:10:50 +0000

    Properties and Encapsulation (with GitHub sync)

--- Library/Book.cs
+++ Library/Book.cs
@@ -6,18 +6,37 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    public class Book
+    class Book
     {
-        public string Title;
-        public string Author;
-        public string ISBN;
+        private string title; // private field
+        private string author; // private field
+        private string isbn; // private field
+
+        //Title property to allow access to the title private field
+        public string Title
+        {
+            get { return title; } // Get Method
+            set { title = value; } // Get Method
+        }
+
+        public string Author
+        {
+            get { return author; } // Get Method
+            set { author = value; } // Get Method
+        }
+
+        public string ISBN
+        {
+            get { return isbn; } // Get Method
+            set { isbn = value; } // Get Method
+        }
 
         // Example of a constructor that allows us to 'construct' a new Book object
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
         }
 
         public void DisplayInfo()
--- Library/Program.cs
+++ Library/Program.cs
@@ -6,11 +6,11 @@ class Program
     {
         // Create a new instance (object) of the book class
         // Note how the object name differs from the class name
-        Book book = new Book("C# for beginners", "BillGates", "12345678");
+        Book book = new Book("C# for Beginners", "BillGates", "1234567");
         book.DisplayInfo();
 
         // This is another new book in our library
-        Book book1 = new Book("C# Methods and Classes", "Microsoft", "55667788");
+        Book book1 = new Book("C# Methods and Classes", "Microsoft", "2233445");
         book1.DisplayInfo();
     }
 }
