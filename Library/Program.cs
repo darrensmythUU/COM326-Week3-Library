e13edcf446cac07fbb1e2ea5d32e8d7e2ce650e8
Kiphea <djsmyth05@gmail.com>
2025-11-08 14:57:16 +0000

    Adding more objects

--- Library/Program.cs
+++ Library/Program.cs
@@ -5,3 +5,9 @@ Book book = new Book();
 book.Title = "C# for beginners";
 book.Author = "BillGates";
 book.ISBN = "12345678";
+
+Book book1 = new Book();
+// This is another new book in our library
+book.Title = "C# Methods and Classes";
+book.Author = "Microsoft";
+book.ISBN = "55667788";
