e927a60a83ec4f4e21b525bc4dd3e97e26e460d6
Kiphea <djsmyth05@gmail.com>
2025-11-09 10:00:25 +0000

    Implementing lists to the Library application

--- Library/Program.cs
+++ Library/Program.cs
@@ -4,21 +4,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
-        Book book = new Book("C# for Beginners", "BillGates", "1234567");
-        Book book1 = new Book("C# Methods and Classes", "Microsoft", "2233445");
-        Console.WriteLine("Currently Available Books");
-        book.DisplayInfo();
-        book1.DisplayInfo();
+        // Create new lists that will be used to store Books and Members
+        List<Book> books = new List<Book>();
+        List<Member> members = new List<Member>();
 
-        // Create a new instances of the Member class
-        // These new members are created using the Member constructor in the Members class
-        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
-        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+        // Create and add new Book objects to the list
+        books.Add(new Book("C# for Beginners", "BillGates", "1234567"));
+        books.Add(new Book("C# Methods and Classes", "Microsoft", "2233445"));
 
-        Console.WriteLine("Current Library Members");
-        member.DisplayInfo();
-        member1.DisplayInfo();
+        members.Add(new Member(1, "John Smith", "1 High Street", 0790090090));
+        members.Add(new Member(2, "Mary Jones", "102 Garden Road", 0790345666));
+
+        // Display all items within the Book List
+        foreach (Book book in books)
+        {
+
+        }
     }
 }
